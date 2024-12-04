fa3258691d8b1cda2720d37e9d20bcf1d09c43d3
Daiane Fernandes <daianedsdaia@gmail.com>
2024-12-04 17:19:32 -0300

    traffic light project

---
+++ README.md
@@ -0,0 +1,11 @@
+# Data Structure
+
+repo created to organize/store my data structure code examples
+## Authors
+
+- [@daianefernandes](https://www.github.com/daianefernandes)
+
+
+## Badges
+
+[![MIT License](https://img.shields.io/badge/License-MIT-green.svg)](https://choosealicense.com/licenses/mit/)
---
+++ trafficLight/index.js
@@ -0,0 +1,24 @@
+const trafficLight = [
+    { light: "red", timer: 30 },
+    { light: "yellow", timer: 20 },
+    { light: "green", timer: 60 },
+  ];
+  
+  let currentIndex = 0;
+  
+  function switchLight() {
+    trafficLight.forEach(({ light }) => {
+      document.getElementById(light).classList.remove("active");
+    });
+  
+    const currentLight = trafficLight[currentIndex];
+    document.getElementById(currentLight.light).classList.add("active");
+  
+    setTimeout(() => {
+      currentIndex = (currentIndex + 1) % trafficLight.length;
+      switchLight();
+    }, currentLight.timer * 1000);
+  }
+  
+  switchLight();
+  
