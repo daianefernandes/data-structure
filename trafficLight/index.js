11f82930d54da35d600c53831cde0a52a2d4bc5e
Daiane Fernandes <daianedsdaia@gmail.com>
2024-12-04 17:48:52 -0300

    traffic light project

--- trafficLight/index.js
+++ trafficLight/index.js
@@ -1,24 +1,23 @@
 const trafficLight = [
-    { light: "red", timer: 30 },
-    { light: "yellow", timer: 20 },
-    { light: "green", timer: 60 },
-  ];
-  
-  let currentIndex = 0;
-  
-  function switchLight() {
-    trafficLight.forEach(({ light }) => {
-      document.getElementById(light).classList.remove("active");
-    });
-  
-    const currentLight = trafficLight[currentIndex];
-    document.getElementById(currentLight.light).classList.add("active");
-  
-    setTimeout(() => {
-      currentIndex = (currentIndex + 1) % trafficLight.length;
-      switchLight();
-    }, currentLight.timer * 1000);
-  }
-  
-  switchLight();
-  
+  { light: "red", timer: 30 },
+  { light: "yellow", timer: 20 },
+  { light: "green", timer: 60 },
+];
+
+let currentIndex = 0;
+
+function switchLight() {
+  trafficLight.forEach(({ light }) => {
+    document.getElementById(light).classList.remove("active");
+  });
+
+  const currentLight = trafficLight[currentIndex];
+  document.getElementById(currentLight.light).classList.add("active");
+
+  setTimeout(() => {
+    currentIndex = (currentIndex + 1) % trafficLight.length;
+    switchLight();
+  }, currentLight.timer * 1000);
+}
+
+switchLight();
